e8739d601cbd91ab7429c9bbef2774355021deaf
Mehara Sahabandu <87423149+it21148118@users.noreply.github.com>
2022-10-28 09:41:28 +0530

    changed main Menu

---
+++ app/src/main/java/com/example/madprojectbrownies/MenuEmp.java
@@ -0,0 +1,14 @@
+package com.example.madprojectbrownies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuEmp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_emp);
+    }
+}
